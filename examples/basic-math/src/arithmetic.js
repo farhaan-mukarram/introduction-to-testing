4c149d3f980d36d8dc5d43545984e3fbf7eb4811
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-09 16:21:19 +0500

    feat: add solution for the basic arithmetic exercise

--- examples/basic-math/src/arithmetic.js
+++ examples/basic-math/src/arithmetic.js
@@ -1,7 +1,41 @@
-export const add = () => {};
+export const add = (a, b) => {
+  if (typeof a !== 'number')
+    throw new Error('The first number must be a number');
 
-export const subtract = () => {};
+  if (typeof b !== 'number')
+    throw new Error('The second number must be a number');
 
-export const multiply = () => {};
+  return a + b;
+};
 
-export const divide = () => {};
+export const subtract = (a, b) => {
+  if (typeof a !== 'number')
+    throw new Error('The first number must be a number');
+
+  if (typeof b !== 'number')
+    throw new Error('The second number must be a number');
+
+  return a - b;
+};
+
+export const multiply = (a, b) => {
+  if (typeof a !== 'number')
+    throw new Error('The first number must be a number');
+
+  if (typeof b !== 'number')
+    throw new Error('The second number must be a number');
+
+  return a * b;
+};
+
+export const divide = (a, b) => {
+  if (typeof a !== 'number')
+    throw new Error('The first number must be a number');
+
+  if (typeof b !== 'number')
+    throw new Error('The second number must be a number');
+
+  if (b === 0) return 0;
+
+  return a / b;
+};
--- examples/basic-math/src/arithmetic.test.js
+++ examples/basic-math/src/arithmetic.test.js
@@ -1,9 +1,62 @@
 import { describe, it, expect } from 'vitest';
+import { add, subtract, divide, multiply } from './arithmetic';
 
-describe.todo('add', () => {});
+describe('add', () => {
+  it('should add two numbers', () => {
+    expect(add(2, 2)).toBe(4);
+  });
 
-describe.todo('subtract', () => {});
+  it('should throw an error when the first argument is not a number', () => {
+    expect(() => add(undefined, 2)).toThrow('must be a number');
+  });
 
-describe.todo('multiply', () => {});
+  it('should throw an error when the second argument is not a number', () => {
+    expect(() => add(2, undefined)).toThrow('must be a number');
+  });
+});
 
-describe.todo('divide', () => {});
+describe('subtract', () => {
+  it('should subtract two numbers', () => {
+    expect(subtract(2, 2)).toBe(0);
+  });
+
+  it('should throw an error when the first argument is not a number', () => {
+    expect(() => subtract(undefined, 2)).toThrow('must be a number');
+  });
+
+  it('should throw an error when the second argument is not a number', () => {
+    expect(() => subtract(2, undefined)).toThrow('must be a number');
+  });
+});
+
+describe('multiply', () => {
+  it('should multiply two numbers', () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+
+  it('should throw an error when the first argument is not a number', () => {
+    expect(() => multiply(undefined, 2)).toThrow('must be a number');
+  });
+
+  it('should throw an error when the second argument is not a number', () => {
+    expect(() => multiply(2, undefined)).toThrow('must be a number');
+  });
+});
+
+describe('divide', () => {
+  it('should divide two numbers', () => {
+    expect(divide(4, 2)).toBe(2);
+  });
+
+  it('should throw an error when the first argument is not a number', () => {
+    expect(() => divide(undefined, 2)).toThrow('must be a number');
+  });
+
+  it('should throw an error when the second argument is not a number', () => {
+    expect(() => divide(2, undefined)).toThrow('must be a number');
+  });
+
+  it('should prevent division by zero and return 0', () => {
+    expect(divide(2, 0)).toBe(0);
+  });
+});
