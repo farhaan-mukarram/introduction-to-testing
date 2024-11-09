c734786113fe74338d5c0eb269780216ce5b6945
Farhaan Mukarram <45497935+farhaan-mukarram@users.noreply.github.com>
2024-11-09 17:28:54 +0500

    feat: add solution for the button exercise

--- examples/element-factory/src/button.test.js
+++ examples/element-factory/src/button.test.js
@@ -1,9 +1,25 @@
+import { screen } from '@testing-library/dom';
+import { userEvent } from '@testing-library/user-event';
 import { createButton } from './button.js';
 
-describe.todo('createButton', () => {
-  it('should create a button element', () => {});
+describe('createButton', () => {
+  beforeEach(() => {
+    document.body.replaceChildren(createButton());
+  });
 
-  it('should have the text "Click Me"', () => {});
+  it('should create a button element', () => {
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+  });
 
-  it('should change the text to "Clicked!" when clicked', async () => {});
+  it('should have the text "Click Me"', () => {
+    const button = screen.getByRole('button');
+    expect(button).toHaveTextContent('Click Me');
+  });
+
+  it('should change the text to "Clicked!" when clicked', async () => {
+    const button = screen.getByRole('button');
+    await userEvent.click(button);
+    expect(button).toHaveTextContent('Clicked!');
+  });
 });
